6d57baa4046ca3546d97f89020a5a0c05d540e98
dumbPirate <shubham_nautiyal@yahoo.com>
2020-05-05 17:15:19 +0530

    added sample database in csv format

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -14,11 +15,31 @@ def submit():
         email = request.form['email']
         phone = request.form['phone']
         birthday = request.form['birthday']
+        gender = request.form['gender']
+        address = request.form['address']
+        state = request.form['state']
+        city = request.form['city']
+        pincode = request.form['pincode']
+        uni_name = request.form['uni_name']
+        field = request.form['field']
+        year = request.form['year']
 
-        print(first_name,last_name, email, phone, birthday)
-        if first_name == '':
-            return render_template('index.html')
+
+
+        fields = [first_name,last_name, email, phone, birthday, gender,
+                 address, state, city, pincode, uni_name, field, year]
+        
+        with open(r'data.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
+
+        # if first_name == '':
+        #     return render_template('index.html', message = 'enter required fields')
         return render_template('success.html')
+
+# fields=['first_name','last_name','email','phone', 'birthday']
+
+
 
 
 if __name__ == '__main__':
